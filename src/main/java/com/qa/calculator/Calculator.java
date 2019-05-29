ddade95c7c152a0273b7206f4f427faa117c6402
HughH1 <hughfharding@gmail.com>
2019-05-29 12:40:02 +0100

    Subtracting feature implemented

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -4,7 +4,14 @@ public class Calculator {
 
 	public int add(int num1, int num2) {
 		return num1 + num2;
-		
 	}
+
+	public double sub(double num1, double num2) {
+		double value;
+		value = num1 - num2;
+
+		return value;
+	}
+	
 
 }
--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -1,15 +1,29 @@
 package com.qa.calculator;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+	Calculator calc;
+
+	@Before
+	public void setup() {
+		calc = new Calculator();
+	}
 
 	@Test
 	public void test() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.add(2, 2);
-		Assert.assertEquals(4, actualValue);
+		double actualValue = calc.add(0.1, 0.1, 0.1);
+		Assert.assertEquals(0.3, actualValue, 0.1);
+	}
+
+	@Test
+	public void subbing() {
+		double actualValue = calc.sub(4, 2);
+		assertEquals("Didn't subtract correctly", 2.0, actualValue, 0.1);
 	}
 
 }
